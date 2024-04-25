91a45dce9696ab248eb2df0a0f263b98629eb92d
Umut <75788371+umutkkozan@users.noreply.github.com>
2024-04-25 12:00:37 +0300

    IslemOnceligi.java
    Aciklamalar eklendi

--- week_1/src/tasks/IslemOnceligi.java
+++ week_1/src/tasks/IslemOnceligi.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 
 public class IslemOnceligi {
     public static void main(String[] args) {
+        //Scanner klavyeden veri girmemiz için kullandığımız bir kütüphane
         Scanner sayiGir = new Scanner(System.in);
         System.out.println("İlk sayıyı giriniz : ");
+        // a - b- c değişkenlerini integer türünde tanımladık
+        // "sayıGir" Scanner sınıfından türettiğimiz bir obje. Objenin sonuna nokta koyarak scanner sınıfının sunduğu nextInt 
+        //ozelliginden faydalanarak girilecek ilk integer degeri a olarak tanımladık
         int a = sayiGir.nextInt();
         System.out.println("İkinci sayıyı giriniz : ");
         int b = sayiGir.nextInt();
         System.out.println("Ucuncu sayıyı giriniz : ");
         int c = sayiGir.nextInt();
+        // islem adındaki double değişkende makinemizin yapmasını istediğimiz formülü tanımladık.
         double islem = a + b * c - b;
+        // Sonucu sout ile yazdırdık.
         System.out.println("Sayıların islem onceliğine gore sonucu : " + islem);
     }
 
