aac48a17ce23112a0d387a7c0590be0acd64be72
Umut <75788371+umutkkozan@users.noreply.github.com>
2024-04-25 12:48:47 +0300

    VucutKitleEndeksi.java
    cm to m

--- week_1/src/tasks/VucutKitleEndeksi.java
+++ week_1/src/tasks/VucutKitleEndeksi.java
@@ -6,14 +6,14 @@ public class VucutKitleEndeksi {
     public static void main(String[] args) {
         Scanner gir = new Scanner(System.in);
         //scanner sınıfı kullanarak kullanıcıdan veri girmesini sağlıyoruz.
-        System.out.print("Boyunuzu giriniz : ");
+        System.out.print("Boyunuzu (metre cinsinden) giriniz : ");
         double boy = gir.nextDouble();
-        // kullanıcının girdiği cm cinsinden boy bilgisini arkaplanda 100'e bölerek metre cinsine çeviriyoruz
-        double boyMetre = boy /100;
+        //  dilersek kullanıcının girdiği veriyi metre değil de cm cinsinden  girmesine izin verip boy bilgisini arkaplanda 100'e bölerek metre cinsine çevirebiliriz
+        //  double boyMetre = boy /100;
         System.out.print("Kilonuzu giriniz : ");
         double kilo = gir.nextDouble();
         //vücut kitle endeksi formülünü vucutKitle değişkeninde tanımlıyoruz.
-        double vucutKitle =( kilo / (boyMetre*boyMetre));
+        double vucutKitle =( kilo / (boy*boy));
         //yazdır
         System.out.print("Vücut kitle endeksiniz :" + vucutKitle);
     }
