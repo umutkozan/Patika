57a018917c366fd0dce85992123673eb912e1767
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-04-26 20:36:15 +0300

    Yeni class oluşturuldu.

---
+++ week_1/src/conditionalstatements/UcakBileti.java
@@ -0,0 +1,32 @@
+package conditionalstatements;
+
+import java.util.Scanner;
+
+public class UcakBileti {
+
+        public static void main(String[] args) {
+            //Değişken yaratımı:
+            Scanner scanner = new Scanner(System.in);
+            double ortaFiyat = 0, yasIndirimi, sonFiyat;
+
+            //Kullanıcı girişleri
+            System.out.println("Mesafeyi km cinsinden giriniz:");
+            double mesafe = scanner.nextDouble();
+            //Negatif sayı kontrolü:
+            if (mesafe <= 0) {
+                System.out.println("Mesafe negatif veya 0 olamaz! Tekrar deneyin.");
+                System.exit(0);
+            }
+
+            System.out.println("Yaşınızı giriniz:");
+            double yas = scanner.nextDouble();
+            //Negatif sayı kontrolü:
+            if (yas < 0) {
+                System.out.println("Yaş negatif olamaz! Tekrar deneyin.");
+                System.exit(0);
+            }
+
+            System.out.println("Yolculuk tipini seçiniz: ( 1 => Tek Yön, 2 => Çift Yön )");
+            int tip = scanner.nextInt();
+        }
+}
