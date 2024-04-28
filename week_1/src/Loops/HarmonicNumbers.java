f6383f758d824cc80b84e616da6b711335662044
umutkkozan <umutkkozan@gmail.com>
2024-04-28 22:24:13 +0300

    isim degisiklikleri yapıldı

--- week_1/src/Loops/HarmonicNumbers.java
+++ week_1/src/Loops/HarmonicNumbers.java
@@ -2,7 +2,7 @@ package Loops;
 
 import java.util.Scanner;
 
-public class ArmstrongNumberChecker {
+public class HarmonicNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Bir sayı girin : ");
