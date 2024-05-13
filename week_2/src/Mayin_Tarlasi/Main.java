ab45e98f21e4cd39b23964876c80bddedac14236
umutkkozan <umutkkozan@gmail.com>
2024-05-13 20:29:29 +0300

    Mayın Tarlası Tamamlandı.

---
+++ week_2/src/Mayin_Tarlasi/Main.java
@@ -0,0 +1,18 @@
+package Mayin_Tarlasi;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int rows,cols;
+        Scanner scanner  = new Scanner(System.in);
+        System.out.println("Mayın tarlasına hoşgeldiniz.!");
+        System.out.println("Lütfen oynamak istediğiniz boyutları seçin. !");
+        System.out.print("Satır sayısı : ");
+        rows = scanner.nextInt();
+        System.out.print("Sütun sayısı : ");
+        cols = scanner.nextInt();
+        MineSweeper game = new MineSweeper(rows,cols);
+
+    }
+}
